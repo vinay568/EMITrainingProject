ac92b13894d8e4852ecd636c6d530d5af6e4f0a7
vinay568 <gopivinay3012@gmail.com>
2022-07-05 17:10:29 +0530

    updating code

--- AmsApi/AmsApi/Services/Interfaces/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using AmsApi.DataModel.Models;
-
-namespace AmsApi.Services.Interfaces
-{
-    public interface ITokenService
-    {
-        public string CreateToken(Employee employee);
-    }
-}
